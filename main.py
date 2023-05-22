24578f14e31c5aa9d695e930e12672ddb5b18dd2
rhushab <rhushabhbontapallee@googlemail.com>
2023-05-22 12:43:38 +0530

    added EXIF analysis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import piexif
 
 def is_photoshopped(image_path):
     image = cv2.imread(image_path)
@@ -12,6 +13,16 @@ def is_photoshopped(image_path):
     # Calculate the Laplacian of the blurred image
     laplacian = cv2.Laplacian(blurred, cv2.CV_64F).var()
 
+    # Read exif data
+    exif_data = piexif.load(image_path)
+
+    # Extract relevant EXIF tags
+    ifd_exif = exif_data.get('Exif', {})
+    software_tag = ifd_exif.get(piexif.ExifIFD.Software)
+    is_manipulated = software_tag is not None and "photoshop" in software_tag.lower()
+
     print(" Chance the image is manipulated: " , laplacian, "out of 100")
+    print("According to the EXIF data analysis this image is surely edited with software: " , is_manipulated)
+    
 image_path = "your_image_path"
 is_photoshopped(image_path)
